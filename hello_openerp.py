3b7d777a488a52e87a5ac05511da8e6e25c206b7
Fábio Cerqueira <cerqueirasfabio@gmail.com>
2011-08-04 14:32:03 -0300

    add simples model

---
+++ hello_openerp.py
@@ -0,0 +1,13 @@
+#-*- coding: utf-8 -*-
+from osv import fields, osv
+
+class hello(osv.osv):
+    """
+    Simple object with one field
+    """
+    _name = 'hello_openerp.hello'
+    _description = 'hello'
+    _columns = {
+        'text':fields.char('text', size=256, required=True, readonly=False),
+    }
+hello()
