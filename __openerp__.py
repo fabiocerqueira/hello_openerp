f9156a119328733ae90597b0cb868bc789dfa201
Fábio Cerqueira <cerqueirasfabio@gmail.com>
2011-08-04 14:22:39 -0300

    add openerp file
    description and basic configuration

---
+++ __openerp__.py
@@ -0,0 +1,17 @@
+{
+    "name" : "HelloOpen",
+    "version" : "0.1",
+    "author" : "Fábio Cerqueira",
+    "category" : "Hello World",
+    "website" : "http://cerqueira.me",
+    "description": """
+        This module is a simple example 
+    """,
+    "depends" : ["base"],
+    "init_xml" : [],
+    "demo_xml" : [],
+    "update_xml" : [],
+    "active": False,
+    "test":[],
+    "installable": True
+}
